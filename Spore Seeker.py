4139611d451c87324ee27ca175793ce27fe765ed
AnimatorOfSouls <fantailfishy@gmail.com>
2019-12-23 20:49:03 +0000

    Updated

--- Spore Seeker.py
+++ Spore Seeker.py
@@ -6,8 +6,6 @@ import random
 working_dir = path.dirname(__file__)
 
 ### CLASSES ###
-
-#Used for the player
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
@@ -49,7 +47,6 @@ class Player(pygame.sprite.Sprite):
 
 
 
-#Enemies
 class Enemy:
     def __init__(self,hp,damage,move,dist,img):
         self.hp = hp
@@ -78,7 +75,7 @@ class Question:
         self.ans = ans
 
     def load(self):
-        pass
+        text_loader(100,300,"Question Time!",self.quest)
 
 
 
@@ -112,6 +109,8 @@ class Weapons:
 
 
 
+
+
 ### Other Modules ###
 #When escape or the exit button is pressed, quit the game
 def quit_game():
@@ -120,11 +119,11 @@ def quit_game():
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
-                running = False
-                pygame.display.quit()
+                pygame.quit()
+                quit()
         elif event.type == QUIT:
-            running = False
-            pygame.display.quit()
+            pygame.quit()
+            quit()
 
     clock.tick(250)
 
@@ -141,6 +140,60 @@ def img(w,h,x,y,file):
 #Changing the background
 def background():
     img(1200,800,0,0,"Background.png")
+
+
+
+#loads a header and the text
+def text_loader(h_pos,m_pos,h_text,m_text):
+    #loading a font to be used. Parameters:(font name, font size)
+    font_big = pygame.font.Font('freesansbold.ttf', 64)
+    font_small = pygame.font.Font('freesansbold.ttf', 32)
+
+    #defining the colour of the text, and the displayed text
+    text_colour = (91,0,14)
+
+    #Creating a rect surface for text and drawing the text on to the surface
+    ##Header
+    header_text = font_big.render(h_text, True, text_colour)
+    headerTextRect = header_text.get_rect()
+    ##Main Text
+    main_text = font_small.render(m_text, True, text_colour)
+    mainTextRect = main_text.get_rect()
+
+    #setting the central location fo the rect
+    headerTextRect.center = (600, h_pos)
+    mainTextRect.center = (600, m_pos)
+
+    #displaying the text
+    screen.blit(header_text, headerTextRect)
+    screen.blit(main_text, mainTextRect)
+    pygame.display.flip()
+
+
+
+#displays the number of points
+def disp_points(points):
+    img(32,32,1050,20,"GUI-Point.png")
+    points = str(points)
+
+    #loading a font to be used. Parameters:(font name, font size)
+    font = pygame.font.Font('freesansbold.ttf', 32)
+
+    #defining the colour of the text, and the displayed text
+    text_colour = (91,0,14)
+
+    #Creating a rect surface for text and drawing the text on to the surface
+    text = font.render(points, True, text_colour)
+    textRect = text.get_rect()
+
+    #setting the central location fo the rect
+    textRect.center = (1125, 36)
+
+    #displaying the text
+    screen.blit(text, textRect)
+    pygame.display.flip()
+
+
 
 
 
@@ -187,7 +240,7 @@ questions = []
 #True answers
 questions.append(Question("In Pokemon, there is a virus called Pokerus that Pokemon can contract.",True))
 questions.append(Question("In Super Mario 64, there are 120 stars to collect.",True))
-questions.append(Question("In Overwatch, Tracer has 40 bullets in her pistols.",True))
+questions.append(Question("In Overwatch, Tracer has exactly 40 bullets in her pistols.",True))
 questions.append(Question("In Stardew Valley, there are 30 bundles in the Community Centre",True))
 
 #False answers
@@ -203,19 +256,21 @@ questions.append(Question("League of Legends was published by Riot Entertainment
 ### Screens ###
 #The start screen with buttons leading to different "pages"
 def start_screen():
-    running = True
     background()
 
     #Loading icon
     img(512,512,180,140,"Game-Icon-Text.png")
 
-    #Loading buttons - y+150 space between each button
-    img(280,110,880,40,"Button-Start.png")     #Start button
-    img(280,110,880,190,"Button-Custom.png")    #Customisation menu
-    img(280,110,880,340,"Button-Scores.png")    #Highscores table
+    #Loading buttons - 20y space between each button
+    img(280,110,900,20,"Button-Start.png")      #Start button
+    img(280,110,900,150,"Button-Custom.png")    #Customisation menu
+    img(280,110,900,280,"Button-Scores.png")    #Leaderboard
+    img(280,110,900,410,"Button-Control.png")   #Controls
+    img(280,110,900,540,"Button-About.png")     #About the game
+    img(280,110,900,670,"Button-Exit.png")      #Exit the game
 
 
-    while running:
+    while True:
         quit_game()
 
         #Checking if a button is pressed, and then running what the button should do
@@ -223,40 +278,49 @@ def start_screen():
         click = pygame.mouse.get_pressed()
 
         #Start game
-        if 880+280>mouse[0]>880 and 40+110>mouse[1]>40 and click[0]==1:
+        if 900+280>mouse[0]>900 and 20+110>mouse[1]>20 and click[0]==1:
             game()
         #Customisation menu
-        wearing = "Hat-Blank.png"   #Removing hats
-        if 880+280>mouse[0]>880 and 190+110>mouse[1]>190 and click[0]==1:
-            custom_screen(wearing)
-        #Highscores table
-        if 880+280>mouse[0]>880 and 340+110>mouse[1]>340 and click[0]==1:
-            highscores_screen()
+        if 900+280>mouse[0]>900 and 150+110>mouse[1]>150 and click[0]==1:
+            custom_screen()
+        #Leaderboard
+        if 900+280>mouse[0]>900 and 280+110>mouse[1]>280 and click[0]==1:
+            leaderboard_screen()
+        #Controls
+        if 900+280>mouse[0]>900 and 410+110>mouse[1]>410 and click[0]==1:
+            controls_screen()
+        #About the game
+        if 900+280>mouse[0]>900 and 540+110>mouse[1]>540 and click[0]==1:
+            about_screen()
+        #Exit the game
+        if 900+280>mouse[0]>900 and 670+110>mouse[1]>670 and click[0]==1:
+            running = False
+            pygame.display.quit()
 
 
 
 #The customisation screen with buttons which can be clicked to change the colour of the player's mushroom
-def custom_screen(wearing):
-    running = True
-
-    #Loading images and background
+def custom_screen():
     background()
 
+    #Loading buttons
     #Variables to input: width, height, x position, y position, file name (in Sprites folder)
     img(280,110,460,600,"Button-Back.png")
     img(128,128,200,145,"GUI-Custom.png")
     img(128,128,200,400,"GUI-Custom.png")
     img(128,128,872,145,"GUI-Custom.png")
     img(128,128,872,400,"GUI-Custom.png")
-    img(78,96,225,161,"Player-Blue.png")
+    img(78,96,225,161,"Player-Red.png")
     img(78,96,225,416,"Player-Pink.png")
     img(78,96,897,161,"Player-Green.png")
     img(78,96,897,416,"Player-Orange.png")
     pygame.display.flip()
 
+    sprite = "Player-Red.png"
+
 
     # Running #
-    while running:
+    while True:
         quit_game()
 
         #Checking if a button is pressed, and then running what the button should do
@@ -270,7 +334,7 @@ def custom_screen(wearing):
 
         #Hat 1
         if 200+128>mouse[0]>200 and 145+128>mouse[1]>145 and click[0]==1:
-            sprite = "Player-Blue.png"
+            sprite = "Player-Red.png"
         #Hat 2
         if 200+128>mouse[0]>200 and 400+128>mouse[1]>400 and click[0]==1:
             sprite = "Player-Pink.png"
@@ -283,16 +347,57 @@ def custom_screen(wearing):
 
 
 
-#The highscores screen with a highscores table and a button to return to the start screen
-def highscores_screen():
-    running = True
+#The leaderboard screen with a highscores table and a button to return to the start screen
+def leaderboard_screen():
     background()
+    text_loader(100,250,"Leaderboard","scores")
 
     #Loading back button
     img(280,110,460,600,"Button-Back.png")
 
+    while True:
+        quit_game()
 
-    while running:
+        #Checking if a button is pressed, and then running what the button should do
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        #Back button
+        if 460+280>mouse[0]>460 and 600+110>mouse[1]>600 and click[0]==1:
+            start_screen()
+
+
+
+#Shows all the controls
+def controls_screen():
+    background()
+    text_loader(100,250,"Controls","press buttons")
+
+    #Loading back button
+    img(280,110,460,600,"Button-Back.png")
+
+    while True:
+        quit_game()
+
+        #Checking if a button is pressed, and then running what the button should do
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        #Back button
+        if 460+280>mouse[0]>460 and 600+110>mouse[1]>600 and click[0]==1:
+            start_screen()
+
+
+
+#A screen showing information about the game
+def about_screen():
+    background()
+    text_loader(100,250,"About the Game","owo/t")
+
+    #Loading back button
+    img(280,110,460,600,"Button-Back.png")
+
+    while True:
         quit_game()
 
         #Checking if a button is pressed, and then running what the button should do
@@ -307,59 +412,62 @@ def highscores_screen():
 
 #The main game
 def game():
-    background()
-
-    running = True
     points = 100                #Player starts with 100 points
 
-    rand_num = random.randint(0,7)
-    question_num = levels[rand_num].levelGen(points)
+    for repeat in range(10):
+        background()
+        disp_points(points)
+        print("Rep num: "+str(repeat))
+
+        rand_num = random.randint(0,7)
+        question_num = levels[rand_num].levelGen(points)
 
 
-    while running:
-        quit_game()
+        while True:
+            quit_game()
 
-        #Checking if a button is pressed, and then running what the button should do
-        mouse = pygame.mouse.get_pos()
-        click = pygame.mouse.get_pressed()
+            #Checking if a button is pressed, and then running what the button should do
+            mouse = pygame.mouse.get_pos()
+            click = pygame.mouse.get_pressed()
 
 
-        #Checking if the current level has an enemy or a question
-        #If enemy, run the fight
-        if levels[rand_num].type == "enemy":
-            pressed_keys = pygame.key.get_pressed()
+            #Checking if the current level has an enemy or a question
+            #If enemy, run the fight
+            if levels[rand_num].type == "enemy":
+                pressed_keys = pygame.key.get_pressed()
 
-            player.update(pressed_keys)
+                player.update(pressed_keys)
 
-            screen.blit(player.image, player.rect)
-            pygame.display.flip()
+                screen.blit(player.image, player.rect)
+                pygame.display.flip()
 
-        #If question, check if player gets the right answer
-        else:
-            #Setting variables to be used
-            player_ans = bool
-            answer = questions[question_num].ans
+            #If question, check if player gets the right answer
+            else:
+                #Setting variables to be used
+                player_ans = bool
+                answer = questions[question_num].ans
 
-            #True button
-            if 220+280>mouse[0]>220 and 500+110>mouse[1]>500 and click[0]==1:
-                player_ans = True
-                break
+                #True button
+                if 220+280>mouse[0]>220 and 500+110>mouse[1]>500 and click[0]==1:
+                    player_ans = True
+                    break
 
-            #False button
-            if 700+280>mouse[0]>700 and 500+110>mouse[1]>500 and click[0]==1:
-                player_ans = False
-                break
+                #False button
+                if 700+280>mouse[0]>700 and 500+110>mouse[1]>500 and click[0]==1:
+                    player_ans = False
+                    break
 
-    #Checking of the player's answer is correct and changing the score accordingly
-    if levels[rand_num].type == "question":
-        if player_ans == answer:
-            points += 25
-            print("correct")
-        else:
-            points -= 10
-            print("wrong")
 
-    print(str(points))
+        #Checking of the player's answer is correct and changing the score accordingly
+        if levels[rand_num].type == "question":
+            if player_ans == answer:
+                points += 25
+                print("correct")
+            else:
+                points -= 15
+                print("wrong")
+
+        pygame.time.wait(50)
 
 
 
@@ -390,5 +498,6 @@ weapons = Weapons()
 ## Running the game
 start_screen()
 #game()
-#custom_screen(wearing)
-#highscores_screen()
+#custom_screen()
+#leaderboard_screen()
+#about_screen()
